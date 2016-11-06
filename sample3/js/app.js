1e5f16e5ffd3497c6d152c6bf879528c551a1cf8
vaio <orangeeeee.ore.ore@gmail.com>
2016-11-06 23:56:12 +0900

    modelの変更により、他のViewの表示を変更する処理を追加。

--- sample3/js/app.js
+++ sample3/js/app.js
@@ -1,43 +1,67 @@
 (function () {
 
-    // Model
+	var Item = Backbone.Model.extend({
+		//見作成
+		defaults: {
+			code: "code",
+			name: "name"
+		}
+	});
+	var item = new Item();
 
-    var Task = Backbone.Model.extend({
-        defaults: {
-            title: "todo sample",
-            completed: false
-        }
-    });
-    
-    var task = new Task();
-    /**/
-    //Marionette.ItemView1の場合、renderが不要になる。
-    var TaskView = Mn.ItemView.extend({
+	//入力エリアのView
+	var ItemInputView = Backbone.View.extend({
+		//ビューで登録できるイベントはelの子要素に限られるから必ずelは指定する。
+		el: "#input-item-code-area",
+
+		events: {
+			'click #updateButton': 'updateItemInfo'
+		},
+		updateItemInfo: function () {
+			console.debug('start updateItemInfo');
+			//ItemInfoViewのエリアを表示させる。
+			//ここではModelの変化によるイベント発火を試したい。
+			let _code = document.getElementById('input-item-code').value;
+			//Modelの値を変更
+			item.set("code", _code);
+			item.set("name", 'iPhone' + _code);
+			
+			/* 以下のようにViewをインスタンス化して呼ぶ必要性はない。
+			console.debug('input code:'+ _code);
+			let _item = new Item({ code: _code, name:"iPhone" + _code });
+			let _view = new ItemInfoView({model:_item});
+			_view.render().el;
+			*/
+		}
+	});
+	var TaskView = Mn.ItemView.extend({
         tagName: 'li',
         events : {"click .command" : "test"},
         test:function() {alert('test');},
         template: '#task-template'
     });
-    
-    /**/
-    /*
-        // BackBone.View
-        var TaskView = Backbone.View.extend({
-            tagName: 'li',
-            events : {"click .command" : "test"},
-            test:function() {alert('test');},
-            template: _.template( $('#task-template').html() ),
-            render: function() {
-                var template = this.template( this.model.toJSON() );
-                this.$el.html(template);
-                return this;
-            }
-        });
-    */
-    var taskView = new TaskView({
-        model: task
-    });
-    console.log(taskView.render().el);
-    $('body').append(taskView.render().el);
-    
+	//表示エリアのView
+	var ItemInfoView = Mn.ItemView.extend({
+		
+		//ビューで登録できるイベントはelの子要素に限られるから必ずelは指定する。
+		el: '#result-area',
+		initialize: function () {
+			this.listenTo(this.model, 'change', this.render);
+		},
+		
+		template: '#item-template' //Marionette Code.
+		
+		/** definition backbone only.
+		template: _.template( $('#item-template').html() ),
+		render: function () {
+			console.debug('ItemInfoView render');
+			
+			var template = this.template( this.model.toJSON() );
+			this.$el.html(template);
+			return this;
+		}
+		**/
+	});
+	new ItemInputView({});
+	new ItemInfoView({model: item});
 })();
